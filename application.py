10c114986ae567eacfad5a7ceaa80db6dc11b03d
Sam Lurye <slurye@Sam-Luryes-MacBook-Pro.local>
2016-11-18 01:06:48 -0500

    Uses the API to get the first 10 restaurant results in Harvard Square, displays them in the list.

--- application.py
+++ application.py
@@ -1,12 +1,23 @@
-from cs50 import SQL
-from flask import Flask, flash, redirect, render_template, request, session, url_for
-from flask_session import Session
-from passlib.apps import custom_app_context as pwd_context
+
+from flask import Flask, flash, redirect, render_template, request, session, url_for, jsonify
+from flask_jsglue import JSGlue
 from tempfile import gettempdir
 import string
+from yelp.client import Client
+from yelp.oauth1_authenticator import Oauth1Authenticator
+
+auth = Oauth1Authenticator(
+    consumer_key = "2XVtPIbmjsdUNWohPrjPNw",
+    consumer_secret = "coHn82hLt5_p9Y97JhsBClknaG4",
+    token = "WH2HhV5L96bTtIY87fPGomyCWqNKwElZ",
+    token_secret = "KO-_ZmFEdWt7SQUWVXS3AFeTPMM"
+)
+
+client = Client(auth)
 
 # configure application
 app = Flask(__name__)
+jsglue = JSGlue(app)
 
 # ensure responses aren't cached
 if app.config["DEBUG"]:
@@ -21,9 +32,17 @@ if app.config["DEBUG"]:
 app.config["SESSION_FILE_DIR"] = gettempdir()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
 
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    results = client.search("Harvard Square", {"term": "food"})
+    results_list = []
+    for i in range(12):
+        results_list.append(results.businesses[i].name)
+    return jsonify(results_list)
+
     
